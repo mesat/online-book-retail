debcd9252c8c9aa17f1e100f191178bb3c1185e9
Muhammet Esat Sakarya <esat.sakarya@pro-line.com.tr>
2021-08-14 18:35:17 +0300

    initial commit

---
+++ readingisgood/src/main/java/org/openreading/readingisgood/controller/BookController.java
@@ -0,0 +1,13 @@
+package org.openreading.readingisgood.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author Muhammet Sakarya
+ * created at 8/14/2021
+ */
+
+@RestController
+public class BookController {
+
+}
---
+++ readingisgood/src/test/java/org/openreading/readingisgood/ReadingisgoodApplicationTests.java
@@ -0,0 +1,13 @@
+package org.openreading.readingisgood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReadingisgoodApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
